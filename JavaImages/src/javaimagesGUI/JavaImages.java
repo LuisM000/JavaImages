ebf4d0f5db930f1d29fdc89505b30ae0d87ba4f5
luis <luis@ubuntu>
2013-09-03 01:46:50 +0200

    Autor: Luis Marcos (luis.m.r@outlook.com) Producto: JavaImages Branch: master Resumen: Include GUI Descripción: se incluye un pequeño adelanto de pruebas de interfaz

---
+++ JavaImages/src/javaimagesGUI/JavaImages.java
@@ -0,0 +1,15 @@
+package javaimagesGUI;
+
+
+/**
+ *
+ * @author Luis
+ */
+public class JavaImages {
+
+
+    public static void main(String[] args) {
+        MainForm screen=new MainForm();
+        screen.setVisible(true);
+    }
+}
